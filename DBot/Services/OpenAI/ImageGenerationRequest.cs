b60efec99b52f96aef309d8fb2eae1a174554d56
Dwi Fuady <dwifuady@gmail.com>
2023-12-07 15:35:16 +0700

    upgrade image generation model

--- DBot/Services/OpenAI/ImageGenerationRequest.cs
+++ DBot/Services/OpenAI/ImageGenerationRequest.cs
@@ -6,11 +6,22 @@ public class ImageGenerationRequest
 {
     public ImageGenerationRequest(string prompt, int n, string size)
     {
+        Model = "dall-e-2";
         Prompt = prompt;
         N = n;
         Size = size;
     }
 
+    public ImageGenerationRequest(string model, string prompt, int n, string size)
+    {
+        Model = model;
+        Prompt = prompt;
+        N = n;
+        Size = size;
+    }
+
+    [JsonPropertyName("model")]
+    public string Model { get; set; }
     [JsonPropertyName("prompt")]
     public string? Prompt { get; }
     [JsonPropertyName("n")]
